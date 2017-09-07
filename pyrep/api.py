d42c9f8cd34e77226f05c0af96d31024c78348db
ASM <cpp.create@gmail.com>
2017-09-07 21:12:37 +0200

    Add some useful functions

--- pyrep/api.py
+++ pyrep/api.py
@@ -34,6 +34,23 @@ class VRepApi:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.simulation.stop()
 
+    def object_handle(api, name):
+        code, obj = v.simxGetObjectHandle(api._id, name, api._def_op_mode)
+        if code != v.simx_return_ok:
+            raise ValueError("{} not found in scene or something bad happened!".format(name))
+        return obj
 
+    def relative_position(self, what, rel_to):
+        code, pos = v.simxGetObjectPosition(self._id, what, rel_to, self._def_op_mode)
+        if code != v.simx_return_ok:
+            raise ValueError("{} not found in scene or something bad happened!".format(name))
+        return pos
 
+    def relative_orientation(self, what, rel_to):
+        code, pos = v.simxGetObjectOrientation(self._id, what, rel_to, self._def_op_mode)
+        if code != v.simx_return_ok:
+            raise ValueError("{} not found in scene or something bad happened!".format(name))
+        return pos
 
+    def set_relative_position(self, what, rel_to, pos):
+        v.simxSetObjectPosition(self._id, what, rel_to, pos, self._def_op_mode)
